1eb5ec48f05a71d4774b2fec91de675e88ea8bf0
Matthew Madurski <matt@matthewmadurski.com>
2015-01-11 22:09:15 -0500

    change to module

--- README.md
+++ README.md
@@ -1,20 +1,40 @@
 ## Quick Start
 
-Run `main.py` at the command line and pass it whichever ticker symbol you so please.
+    import options_scraper
+
+    options_scraper.get_options_data('XON')
+
 You will get back a dictionary of the following form:
 
     {
-        expiration_date: {
+        expiration_date1: {
             'puts': {
                 ...
             },
             'calls': {
                 ...
             }
-        }
+        },
+        expiration_date2: {
+            ...
+        },
+        ...
     }
 
 where `expiration_date` is a python `datetime` object.
 
-`'puts'` and `'calls'` will contain `key/value` pairs for all
-the available data from yahoo.
+The `'puts'` and `'calls'` dictionaries will contain
+`key/value` pairs for all the available data from yahoo,
+which at the time is:
+    'strike',
+    'name',
+    'last',
+    'bid',
+    'ask',
+    'change',
+    '%change',
+    'volume',
+    'interest',
+    'volatility'
+
+All values are python strings.
--- options_scraper.py
+++ options_scraper.py
@@ -21,28 +21,26 @@ CONTRACT_KEYS = [
 ]
 
 
-def get_soup(url):
+def _get_soup(url):
     html_page = requests.get(url).content
     return BeautifulSoup(html_page, 'lxml')
 
-def get_contract_query_params(ticker):
+def _get_contract_query_params(ticker):
     url = BASE_URL.format(ticker)
-    soup = get_soup(url)
+    soup = _get_soup(url)
     contract_list = soup.find('select', class_='Start-0')
     contract_soup = BeautifulSoup(str(contract_list), 'lxml')
     contract_options_elements = contract_soup.find_all('option')
     contracts = {}
     for contract in contract_options_elements:
-        date = contract.text
-        formatted_date = datetime.strptime(date, '%B %d, %Y')
+        formatted_date = datetime.strptime(contract.text, '%B %d, %Y')
         contracts[formatted_date] = contract['value']
     return contracts
 
-def get_particular_option_data(table):
+def _get_particular_option_data(table):
     contracts = []
     soup = BeautifulSoup(str(table.tbody), 'lxml')
-    table_rows = soup.find_all('tr')
-    for row in table_rows:
+    for row in soup.find_all('tr'):
         contract_dict = {}
         for i, cell in enumerate([x for x in row.contents if x != '\n']):
             cell = BeautifulSoup(str(cell), 'lxml')
@@ -50,24 +48,19 @@ def get_particular_option_data(table):
         contracts.append(contract_dict)
     return contracts
 
-def get_options_data_for_contract(ticker, contract):
+def _get_options_data_for_contract(ticker, contract):
     url = '{}&date={}'.format(BASE_URL.format(ticker), contract)
-    soup = get_soup(url)
+    soup = _get_soup(url)
     table = soup.find_all('table', class_='quote-table')
     return {
-        'calls': get_particular_option_data(table[0]),
-        'puts': get_particular_option_data(table[1])
+        'calls': _get_particular_option_data(table[0]),
+        'puts': _get_particular_option_data(table[1])
     }
 
 
-def main():
-    _, ticker = argv
-    contracts = get_contract_query_params(ticker)
+def get_options_data(ticker):
+    contracts = _get_contract_query_params(ticker)
     data = {}
     for date, name in contracts.iteritems():
-        data[date] = get_options_data_for_contract(ticker, name)
+        data[date] = _get_options_data_for_contract(ticker, name)
     return data
-
-
-if __name__ == '__main__':
-    main()
