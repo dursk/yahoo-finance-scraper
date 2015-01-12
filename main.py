22e810bec8cd9720ea545ea09db15b90e34d262f
Matthew Madurski <matt@matthewmadurski.com>
2015-01-11 21:53:13 -0500

    get all exp dates & refactor

--- main.py
+++ main.py
@@ -20,12 +20,15 @@ CONTRACT_KEYS = [
     'volatility'
 ]
 
-def main():
-    _, ticker = argv
+
+def get_soup(url):
+    html_page = requests.get(url).content
+    return BeautifulSoup(html_page, 'lxml')
+
+def get_contract_query_params(ticker):
     url = BASE_URL.format(ticker)
-    html = requests.get(url).content
-    main_soup = BeautifulSoup(html, 'lxml')
-    contract_list = main_soup.find('select', class_='Start-0')
+    soup = get_soup(url)
+    contract_list = soup.find('select', class_='Start-0')
     contract_soup = BeautifulSoup(str(contract_list), 'lxml')
     contract_options_elements = contract_soup.find_all('option')
     contracts = {}
@@ -33,40 +36,39 @@ def main():
         date = contract.text
         formatted_date = datetime.strptime(date, '%B %d, %Y')
         contracts[formatted_date] = contract['value']
+    return contracts
 
-    table = main_soup.find_all('table', class_='quote-table')
+def get_particular_option_data(table):
+    contracts = []
+    soup = BeautifulSoup(str(table.tbody), 'lxml')
+    table_rows = soup.find_all('tr')
+    for row in table_rows:
+        contract_dict = {}
+        for i, cell in enumerate([x for x in row.contents if x != '\n']):
+            cell = BeautifulSoup(str(cell), 'lxml')
+            contract_dict[CONTRACT_KEYS[i]] = cell.text[1:-1]
+        contracts.append(contract_dict)
+    return contracts
+
+def get_options_data_for_contract(ticker, contract):
+    url = '{}&date={}'.format(BASE_URL.format(ticker), contract)
+    soup = get_soup(url)
+    table = soup.find_all('table', class_='quote-table')
     call_table = table[0]
     put_table = table[1]
+    return {
+        'calls': get_particular_option_data(call_table),
+        'puts': get_particular_option_data(put_table)
+    }
 
-    put_contracts = []
 
-    put_table_soup = BeautifulSoup(str(put_table.tbody), 'lxml')
-    table_rows = put_table_soup.find_all('tr')
-
-    for row in table_rows:
-        contract_dict = {}
-
-        for i, cell in enumerate([x for x in row.contents if x != '\n']):
-            cell = BeautifulSoup(str(cell), 'lxml')
-            contract_dict[CONTRACT_KEYS[i]] = cell.text[1:-1]
-
-        put_contracts.append(contract_dict)
-
-    call_contracts = []
-
-    call_table_soup = BeautifulSoup(str(call_table.tbody), 'lxml')
-    table_rows = call_table_soup.find_all('tr')
-
-    for row in table_rows:
-        contract_dict = {}
-
-        for i, cell in enumerate([x for x in row.contents if x != '\n']):
-            cell = BeautifulSoup(str(cell), 'lxml')
-            contract_dict[CONTRACT_KEYS[i]] = cell.text[1:-1]
-
-        call_contracts.append(contract_dict)
-
-    return {'calls': call_contracts, 'puts': put_contracts}
+def main():
+    _, ticker = argv
+    contracts = get_contract_query_params(ticker)
+    data = {}
+    for date, name in contracts.iteritems():
+        data[date] = get_options_data_for_contract(ticker, name)
+    print data
 
 
 if __name__ == '__main__':
