f8e4a108464488d1a04cedb5904f1986586a6a83
Matthew Madurski <matt@matthewmadurski.com>
2015-01-20 21:12:31 -0500

    StockScraper to support multiple tickers

--- scraper.py
+++ scraper.py
@@ -57,35 +57,44 @@ class StockScraper(BaseScraper):
         return stock
 
     def _write_csv_headers(self, writer):
-        header_row = ['current']
+        header_row = ['ticker', 'current']
         for sub_list in self.STOCK_KEYS:
             header_row.extend(sub_list)
         writer.writerow(header_row)
 
-    def _write_csv_data_row(self, writer):
-        data = self.get_data()
-        row = [data['current']]
-        for sub_list in self.STOCK_KEYS:
-            row.extend([data[i] for i in sub_list])
-        writer.writerow(row)
+    def _write_csv_data_rows(self, writer, tickers):
+        data = self.get_data(tickers)
+        for ticker in data:
+            ticker_data = data[ticker]
+            row = [ticker, ticker_data['current']]
+            for sub_list in self.STOCK_KEYS:
+                row.extend([ticker_data[i] for i in sub_list])
+            writer.writerow(row)
 
     def _get_current_price(self, soup):
         return soup.find('span', class_='time_rtq_ticker').text
 
-    def get_data(self, ticker):
-        url = self._get_url(ticker)
-        soup = self._get_soup(url)
-        data = self._parse_table(soup)
-        data['current'] = self._get_current_price(soup)
-        return {ticker: data}
+    def get_data(self, tickers):
+        if type(tickers) is str:
+            tickers = [tickers]
+        data = {}
+        for ticker in tickers:
+            url = self._get_url(ticker)
+            soup = self._get_soup(url)
+            ticker_data = self._parse_table(soup)
+            ticker_data['current'] = self._get_current_price(soup)
+            data[ticker] = ticker_data
+        return data
 
-    def export_to_csv(self, ticker, filename=None):
+    def export_to_csv(self, tickers, filename=None):
+        if type(tickers) is str:
+            tickers = [tickers]
         if not filename:
-            filename = '{}.csv'.format(ticker)
+            filename = 'stock_data.csv'
         with open(filename, 'w+') as f:
             writer = csv.writer(f)
             self._write_csv_headers(writer)
-            self._write_csv_data_row(writer)
+            self._write_csv_data_rows(writer, tickers)
 
 
 class OptionsScraper(BaseScraper):
