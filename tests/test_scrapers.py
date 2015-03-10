ebb3be032ec622568a316e97ef938afa99f5ddc3
Matthew Madurski <matt@matthewmadurski.com>
2015-03-09 20:37:07 -0400

    add basic OptionScraper test case

--- tests/test_scrapers.py
+++ tests/test_scrapers.py
@@ -1,24 +1,40 @@
 import unittest
 
 from scrapers.stocks import StockScraper
+from scrapers.options import OptionScraper
 
 from bs4 import BeautifulSoup
 
 
+TICKER = 'XON'
+
+
 class StockScraperTestCase(unittest.TestCase):
 
-    TEST_STOCK = 'GOOG'
-
     def test_get_data(self):
-        data = StockScraper().get_data(self.TEST_STOCK)
-        self.assertIn(self.TEST_STOCK, data)
+        data = StockScraper().get_data(TICKER)
+        self.assertIn(TICKER, data)
 
-        stock = data[self.TEST_STOCK]
+        stock = data[TICKER]
         keys = StockScraper.STOCK_KEYS[0] + StockScraper.STOCK_KEYS[1]
 
         for key in keys:
             self.assertIn(key, stock)
             self.assertIsNotNone(stock.get(key, None))
+
+
+class OptionScraperTestCase(unittest.TestCase):
+
+    def test_get_data(self):
+        data = OptionScraper().get_data(TICKER)
+
+        underlying = data.pop('underlying')
+        self.assertIsNot(underlying, '')
+
+        for exp_date in data.values():
+            for option in exp_date['calls'] + exp_date['puts']:
+                for key in OptionScraper.CONTRACT_KEYS:
+                    self.assertIn(key, option)
 
 
 if __name__ == '__main__':
